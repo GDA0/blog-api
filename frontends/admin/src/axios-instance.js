208c51dfbd1ac8f31021bbc7e9d073f5879f04b9
GDA0 <gideondelaliadeti0@gmail.com>
2024-08-09 11:46:11 +0000

    Setup admin route

---
+++ frontends/admin/src/axios-instance.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// Create an Axios instance
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api/admin",
+});
+
+export default axiosInstance;
--- frontends/admin/src/components/routes/Root.jsx
+++ frontends/admin/src/components/routes/Root.jsx
@@ -1,13 +1,54 @@
 import { Header } from "../Header";
 import { Footer } from "../Footer";
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "../../axios-instance";
 
 export function Root() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("/");
+        const { posts } = response.data;
+        setPosts(posts);
+        setError("");
+      } catch (error) {
+        console.error(error);
+        setError("Something went wrong.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
       <main className="container my-5 py-3">
-        <Outlet />
+        {error && (
+          <div className="text-center mx-auto" style={{ maxWidth: "420px" }}>
+            <div className="alert alert-danger" role="alert">
+              {error}
+            </div>
+          </div>
+        )}
+
+        {loading && (
+          <div className="text-center">
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )}
+
+        <Outlet context={[posts]} />
       </main>
       <Footer />
     </>
